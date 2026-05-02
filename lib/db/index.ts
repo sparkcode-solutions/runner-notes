c8cd3b2c4a5010e5b8e940913521121ef13cd7fb
bishesh-panthos <bishesh@panthos.com.au>
2026-05-02 14:42:36 +0545

    chore : update readme to ad

--- lib/db/index.ts
+++
@@ -1,7 +0,0 @@
-// Database exports
-export { db, DATABASE_NAME } from './client';
-export { DatabaseProvider, useDatabase } from './provider';
-export { runMigrations } from './migrations';
-
-// Schema exports
-export * from './schema';
--- lib/hooks/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './use-trails';
-export * from './use-run-moments';
-export * from './use-journals';
-export * from './use-run-snaps';
